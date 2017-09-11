95de96c9b8f79efa87d474c78d858ce95861ab20
David Schell <davidschell@shaw.ca>
2017-09-10 19:13:57 -0700

    Add navbar and disclaimer page.

--- StockChart/ui.R
+++ StockChart/ui.R
@@ -14,29 +14,37 @@ shinyUI(fluidPage(theme = shinytheme("cerulean"),
   # Application title
   titlePanel("Techncial Chart Viewer"),
   
-  sidebarLayout(
+  navbarPage("Menu:",
+    tabPanel("Home",
+             
+      sidebarLayout(
     
-    sidebarPanel(
-      "Enter Symbol With Extension:", 
-      textInput("symbol", "TSX = .T,  TSXV = .V, CSE = .C", "TD.T",width="400px"),
-      actionButton(inputId="btnChart", label = "Display Chart"),
-      tags$br(),tags$br(),
-      checkboxGroupInput(inputId = "indicators", label = "Indicators",choices = getTAList())
-    ),
+        sidebarPanel(
+          "Enter Symbol With Extension:", 
+          textInput("symbol", "TSX = .T,  TSXV = .V, CSE = .C", "TD.T",width="400px"),
+          actionButton(inputId="btnChart", label = "Display Chart"),
+          tags$br(),tags$br(),
+          checkboxGroupInput(inputId = "indicators", label = "Indicators",choices = getTAList())
+        ),
     
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Chart",withSpinner(plotOutput("chart",width="100%",height = "700px"))), 
-        tabPanel("Summary", 
+        mainPanel(
+          tabsetPanel(
+            tabPanel("Chart",withSpinner(plotOutput("chart",width="100%",height = "700px"))), 
+            tabPanel("Summary", 
                  tags$br(),
                  verbatimTextOutput("companyName"),
                  verbatimTextOutput("sharesOS"),
                  verbatimTextOutput("averageDailyVolume")
                  ), 
-        tabPanel("Table", tableOutput("table"))
+            tabPanel("Table", tableOutput("table"))
+          )
+        )
       )
-
-    
+    ),
+    tabPanel("Daily List"),
+    tabPanel("Disclaimer",
+             tags$p(),
+             titlePanel("This website and the information contained herein is not intended to be a source of advice or creditable analysis. The website is for the sole purpose of demonstrating the capabilities of R Shiny applications.")
     )
   )
 ))
